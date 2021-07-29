7d79724fbc61c627d965815eb7bd2e231662a347
Ayushmaan sharma <55322319+ayushman999@users.noreply.github.com>
2021-07-30 03:15:52 +0530

    added otp verification and setup profile

--- app/src/main/java/com/ayushman999/baatein/PhoneNumberActivity.java
+++ app/src/main/java/com/ayushman999/baatein/PhoneNumberActivity.java
@@ -2,13 +2,24 @@ package com.ayushman999.baatein;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class PhoneNumberActivity extends AppCompatActivity {
+import com.ayushman999.baatein.databinding.ActivityPhoneNumberBinding;
 
+public class PhoneNumberActivity extends AppCompatActivity {
+    ActivityPhoneNumberBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_number);
+        getSupportActionBar().hide();
+        binding=ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.continueNumber.setOnClickListener(v->{
+            Intent transfer=new Intent(PhoneNumberActivity.this,OTPActivity.class);
+            transfer.putExtra("Phone",binding.enterNumber.getText().toString().trim());
+            startActivity(transfer);
+        });
     }
 }
